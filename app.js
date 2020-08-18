36ca46ad96c09a9175ddb522ea011331b8d9f534
anupamroy8 <anupamroy8@gmail.com>
2020-08-18 18:26:48 +0530

    mongodb cluster added

--- app.js
+++ app.js
@@ -1,51 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require("mongoose")
-var session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 var auth = require("./middlewares/auth");
 var passport = require("passport");
 var flash = require("connect-flash");
-var stripe = require("stripe")("sk_test_51GqjRpIOBWTLG7Fdo9VtAyWRnmUY3JB4s07BhpEzWBsiw3G8aH2aCtL7sADAkZTK7THOeiYAdcl2aZSooDvK8RCM00tonaKCnH");
+var stripe = require("stripe")(
+  "sk_test_51GqjRpIOBWTLG7Fdo9VtAyWRnmUY3JB4s07BhpEzWBsiw3G8aH2aCtL7sADAkZTK7THOeiYAdcl2aZSooDvK8RCM00tonaKCnH"
+);
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 var shopsrouter = require("./routes/shops");
 var productsrouter = require("./routes/products");
 
-
 // DB connect
-mongoose.connect("mongodb://localhost:27017/shopping-cart", { useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
-    console.log("connected", err ? err: true);   
-});
+mongoose.connect(
+  "mongodb+srv://admin:myadmin@123@cluster0.h5oil.mongodb.net/shopping-cart?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log("connected", err ? err : true);
+  }
+);
 
 // dotenv & passport
 require("dotenv").config();
-require("./modules/passport")
+require("./modules/passport");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// SESSION 
-app.use(session({
-  secret: "mysecret",
-  resave: true,
-  saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
-}))
+// SESSION
+app.use(
+  session({
+    secret: "mysecret",
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 // passport initialize
 app.use(passport.initialize());
@@ -54,27 +60,26 @@ app.use(passport.session());
 
 // routers
 app.use(flash());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use(auth.checkUserLogged);
 app.use(auth.UserInfo);
-app.use('/shops', shopsrouter);
-app.use('/products', productsrouter);
-
+app.use("/shops", shopsrouter);
+app.use("/products", productsrouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
